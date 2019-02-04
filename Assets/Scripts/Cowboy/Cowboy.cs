a7322d45f0cc87af1d346daca6b6feaaa2850cc5
Unknown <lean1998@hotmail.com>
2019-02-04 17:09:15 -0300

    InspectorCollliderGenerator

--- Assets/Scripts/Cowboy/Cowboy.cs
+++ Assets/Scripts/Cowboy/Cowboy.cs
@@ -10,17 +10,37 @@ public class Cowboy : MonoBehaviour
     private Vector3[] baseVertices;
     public Vector3 colliderScale = Vector3.one / 3;
     // Use this for initialization
-    void Start()
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void InvokeDie()
+    {
+        Invoke("Die", 3.0f);
+    }
+
+    public void Die()
+    {
+        Debug.Log("Hola");
+        gameObject.SetActive(false);
+    }
+
+
+    public void GenerateCollider()
     {
         meshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
         meshCollider = GetComponentInChildren<MeshCollider>();
         Mesh colliderMesh = new Mesh();
         meshRenderer.BakeMesh(colliderMesh);
 
-        if (baseVertices == null)
+       // if (baseVertices == null)
             baseVertices = colliderMesh.vertices;
         Vector3[] vertices = new Vector3[baseVertices.Length];
-        for (var i = 0; i < vertices.Length; i++)
+        for (int i = 0; i < vertices.Length; i++)
         {
             Vector3 vertex = baseVertices[i];
             vertex.x = vertex.x * colliderScale.x;
@@ -38,22 +58,5 @@ public class Cowboy : MonoBehaviour
         meshCollider.sharedMesh = colliderMesh;
 
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void InvokeDie()
-    {
-        Invoke("Die", 3.0f);
-    }
-
-    public void Die()
-    {
-        Debug.Log("Hola");
-        gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/Cowboy/Editor/Editor_Cowboy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(Cowboy))]
+public class Editor_Cowboy : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Cowboy cowboy = (Cowboy)target;
+
+        if (GUILayout.Button("Generate collider"))
+        {
+            cowboy.GenerateCollider();
+        }
+    }
+}

68ed7286d9bb86283b4327efbee7815e73c31b5e
Unknown <lean1998@hotmail.com>
2019-02-04 22:48:24 -0300

    FpsDebugger

---
+++ Assets/Scripts/Utilities/FPSDebugger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSDebugger : MonoBehaviour
+{
+    private int frameCount = 0;
+    private float dt = 0.0F;
+    private float fps = 0.0F;
+
+    [Header("UpdateRateSeconds")]
+    [Space(10)]
+    public float updateRateSeconds = 4.0F;
+    [Header("TextFormat")]
+    [Space(10)]
+    public string formatedString = "{value} FPS";
+    [Header("Text")]
+    [Space(10)]
+    public Text txtFps;
+
+
+    void Update()
+    {
+        frameCount++;
+        dt += Time.unscaledDeltaTime;
+        if (dt > 1.0 / updateRateSeconds)
+        {
+            fps = frameCount / dt;
+            frameCount = 0;
+            dt -= 1.0F / updateRateSeconds;
+        }
+        txtFps.text = formatedString.Replace("{value}", System.Math.Round(fps, 1).ToString("0.0"));
+    }
+}

f3e2fa112561a8973d7971fb3361299164b49599
Unknown <lean1998@hotmail.com>
2019-02-13 12:14:43 -0300

    LoadingCircle

--- Assets/Scripts/UI/UI_MainMenu.cs
+++ Assets/Scripts/UI/UI_MainMenu.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class UI_MainMenu : MonoBehaviour {
 
     public Animator fadeAnimator;
+    public Image loadingCircle;
+    private AsyncOperation asyncLoading;
 	// Use this for initialization
 	void Start () {
-		
+ 
 	}
 	
 	// Update is called once per frame
@@ -23,7 +26,21 @@ public class UI_MainMenu : MonoBehaviour {
 
     public void ChangeScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadAsyncScene(sceneName));
     }
 
+
+  
+
+    IEnumerator LoadAsyncScene(string sceneName)
+    {
+        asyncLoading = SceneManager.LoadSceneAsync(sceneName);
+        loadingCircle.gameObject.SetActive(true);
+        while (!asyncLoading.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncLoading.progress / 0.9f);
+            loadingCircle.fillAmount = progress;
+            yield return new WaitForSeconds(1f);
+        }
+    }
 }

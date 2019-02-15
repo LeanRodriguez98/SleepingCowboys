616b52a474e1e0b09b52f9ea5cdfa6e652d4021f
Unknown <lean1998@hotmail.com>
2019-02-14 22:40:46 -0300

    BackGroundMusic

---
+++ Assets/Scripts/Utilities/BackGroundMusic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMusic : MonoBehaviour {
+
+    private static BackGroundMusic instance;
+    private AudioSource audioSource;
+    public AudioClip audioClip;
+    [Range(0.0f,1.0f)] public float volume;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+            instance = this;
+        else
+            gameObject.SetActive(false);
+    }
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = audioClip;
+        audioSource.loop = true;
+        audioSource.volume = volume;
+        audioSource.spatialBlend = 0;
+        audioSource.Play();
+    }
+	
+	
+}

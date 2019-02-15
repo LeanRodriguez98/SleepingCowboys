880d30940c00da979909eb61f34db168b26e5f92
Unknown <lean1998@hotmail.com>
2019-02-15 13:45:11 -0300

    PlayrVRPause

---
+++ Assets/Scripts/UI/UI_Pause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Pause : MonoBehaviour {
+
+    public GameObject pauseButton;
+    public Image buttonImage;
+    public Sprite PauseSprite;
+    public Sprite PlaySprite;
+    private void Start()
+    {
+        buttonImage.sprite = PauseSprite;
+
+    }
+
+    public void Pause()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            buttonImage.sprite = PauseSprite;
+
+        }
+        else if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            buttonImage.sprite = PlaySprite;
+
+        }
+        
+    }
+
+  
+}
